ef456cca57b937c063bae32b2e645e6ecc64c255
Jobanpreet Singh <210142187@aston.ac.uk>
2023-02-25 19:58:17 +0000

    updated the navbar for the user and admin login

--- team33/resources/views/nav&footer/nav.blade.php
+++ team33/resources/views/nav&footer/nav.blade.php
@@ -19,11 +19,34 @@
             </li>
             <li><a href="{{('contact')}}" class="nav-txt">Contact Us</a></li>
             <li><a href="{{('about')}}" class="nav-txt">About Us</a></li>
-        </ul>
-        <ul class="topnav">
-            <li><a href="{{('login')}}" class="nav-txt">Log in</a></li>
-            <li><a href="{{('register')}}" class="nav-txt">Register</a></li>
-        </ulß>
+            @guest <!-- if user is loged in, this will not appear  -->
+      <li><a href="{{ route('login') }}" class="nav-txt">Log in</a></li>
+      <li><a href="{{ route('register') }}" class="nav-txt">Register</a></li>
+      @endguest
+        
+        
+
+      <?php if(@Auth::user()->name != null) : ?>
+      <?php if(@Auth::user()->name != 'Admin') : ?>
+      <?php if(auth()->user()->type ?? 2): ?>
+        <li><a href="{{url('userAccount')}}" class="nav-txt">Account</a></li>
+        <li><a href="{{ route('logout') }}" class="nav-txt">logout</a></li>
+         
+      
+      <?php endif; ?>
+      <?php endif; ?>
+      <?php endif; ?>
+      
+
+      
+      <?php if(@Auth::user()->name == 'Admin') : ?>
+      <?php if(auth()->user()->type ?? 0): ?>
+        <li><a href="{{url('adminAccount')}}" class="nav-txt">Account</a></li>
+        <li><a href="{{ route('logout') }}" class="nav-txt">logout</a></li>
+
+      <?php endif; ?>
+      <?php endif; ?>
+      </ul>
     </section>
 </nav>
 
