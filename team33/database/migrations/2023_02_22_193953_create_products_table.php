e40ec0106ae53d93a0889f5a229644c34222084b
Jobanpreet Singh <210142187@aston.ac.uk>
2023-03-01 15:52:41 +0000

    updated the products table to store its category

--- team33/database/migrations/2023_02_22_193953_create_products_table.php
+++ team33/database/migrations/2023_02_22_193953_create_products_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('description');
             $table->integer('quantity');
             $table->string('image');
+            $table->string('category');
         });
     }
 
