a36dfae2eaa4459d589efe1909b9ff39296f007e
Jobanpreet Singh <210142187@aston.ac.uk>
2023-03-01 15:58:21 +0000

    updates to get data from database in user- admin page

--- team33/app/Http/Controllers/HomeController.php
+++ team33/app/Http/Controllers/HomeController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Models\User;
+use App\Models\Basket;
+use App\Models\Products;
+use App\Models\UserOrders;
+use App\Models\UserQueries;
+
 
 class HomeController extends Controller
 {
@@ -25,12 +30,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('userAccount');
+        
+       $basket_orders = Basket::where('user_id', Auth::id())->get();  #get basket items where field 'user-id' in basket table is == to Auth::id
+       $orders_list = UserOrders::where('user_id', Auth::id())->get();
+       $total = Basket::where('user_id', Auth::id())->sum('price');
+
+
+        return view('userAccount', compact('basket_orders', 'total','orders_list')); # return /home + stored data
     }
 
     public function admin()
     {
-        return view('adminAccount');
+        $store = User::all(); #store users in $store
+        $products = Products::all(); #store products in this variable
+        $orders = UserOrders::all(); #store products in this variable
+        $queries = UserQueries::all(); #store products in this variable
+        return view('adminAccount', ['users' => $store], compact('products','orders','queries','store')); #memebers is a key that can be used as a variables in html page using $members
+        
     }
 
     public function logout(Request $request) {
