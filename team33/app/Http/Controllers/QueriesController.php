46125f1df3c69d94abf2b3095a89d72a13953ad3
Jobanpreet Singh <210142187@aston.ac.uk>
2023-03-01 15:50:42 +0000

    fetch data from contact page to store in database

--- team33/app/Http/Controllers/QueriesController.php
+++ team33/app/Http/Controllers/QueriesController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserQueries;
 
 class QueriesController extends Controller
 {
-    //
+    public function contactForm(Request $request){ #fetch data from contact form and save into database table queries
+
+        $query = new UserQueries();
+    
+        $query->email=$request->email1;
+        $query->name=$request->name1;
+        $query->query=$request->query1;
+        $query->save();
+    
+        return redirect('contact');
+        
+        }
 }
