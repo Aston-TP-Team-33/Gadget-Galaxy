b07f74ce54b933be7d7f6c0a2f603b0dbf979ef9
Jobanpreet Singh <210142187@aston.ac.uk>
2023-03-01 15:48:22 +0000

    worked on backend to display products in stock

--- team33/app/Http/Controllers/ProductsController.php
+++ team33/app/Http/Controllers/ProductsController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Products;
+use Auth;
+use App\Models\User;
+use App\Models\Basket;
+use App\Models\UserOrders;
+use App\Models\UserQueries;
+use DB;
+
+
 
 class ProductsController extends Controller
 {
-    //
+    public function index()
+    {
+        $min = 1;
+        $storeData = Products::where('quantity', '>=', $min)->get();
+        #$storeData = Products::all(); #store data from table
+        return view('product', compact('storeData')); # bring data into the products page
+        
+    }
+
+    #admin updatequantity
+    public function updatequantity(Request $request, $id){
+
+        DB::table('products')
+        ->where('id', $id)
+        ->update(array('quantity' => $request->quantity));
+    
+        return redirect('/adminAccount');
+       
+        }
 }
