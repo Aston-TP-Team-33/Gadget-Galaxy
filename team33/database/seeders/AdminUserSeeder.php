eefcaaa5b64910ca021974e44d5ab8d3134e326c
Jobanpreet Singh <210142187@aston.ac.uk>
2023-03-01 15:53:22 +0000

    seeder to ease export of data to database

--- team33/database/seeders/AdminUserSeeder.php
+++ team33/database/seeders/AdminUserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Products;
 
 class AdminUserSeeder extends Seeder
 {
@@ -28,5 +29,26 @@ class AdminUserSeeder extends Seeder
         foreach ($users as $key => $user) {
             User::create($user);
         }
-    }
+
+        $products = [
+            [
+
+               'title'=>'Facebook',
+
+               'price'=> 10,
+               'description'=>'This is a demo product',
+               'quantity'=> 15,
+               'category'=> 'Smartphones',
+               'image'=>'images/facebook.png',
+            ],
+
+            
+        ];
+    
+        foreach ($products as $key => $product) {
+            Products::create($product);
+        }
+    
+}
+
 }
