615785c64ad80885aa937c94de164bbe566c72ed
Sam Rogers <samrogers900@gmail.com>
2023-03-13 23:23:15 +0000

    created the css for the products page

--- team33/resources/views/product.blade.php
+++ team33/resources/views/product.blade.php
@@ -12,44 +12,30 @@
 
 </head>
 
-<body>
+<body class="product-page">
     @include('nav&footer/nav')
-
-    <h1>Products Page</h1>
+    <section class="product-page-space" style="background-image: url('images/Spacebackground2.gif');">
+        <h1 class="product-page-header">Products Page</h1>
+</section>
     <div class="product-container">
+        @foreach($storeData as $product)    <!-- 1 -->
+            <div class="product">
+                <img src="{{$product->image}}" alt="" class="product-image">  
+                <!--  <img width="200px" src="images/facebook.png" alt="product image"> -->
+                <h2 class="product-title"> {{$product->title}}</h2> 
+                <p class="product-desc"> {{$product->description}}</p>
+                <p2 class="product-quantity">Quantity: {{$product->quantity}}</p2>
+                <p3 class="product-price">Price: £{{$product->price}}</p3>
 
-    <div class="product">
-        <img width="200px" src="images/facebook.png" alt="product image">
-        <h2>product title </h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-        <p2>cost </p2>
-        <p3>category</p3>
-        <form id="item" method="post"> </form>
-    </div>
+                <form action="{{url('home',$product->id)}}" method="POST">
+            @csrf
+            <input type= "number" value="1" min="1" max="{{$product->quantity}}" class="product-quantity" name="quantity">
 
-    <div class="product">
-        <img width="200px" src="images/facebook.png" alt="product image">
-        <h2>product title </h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-        <p2>cost </p2>
-        <p3>category</p3>
-        <form id="item" method="post"> </form>
-    </div>
+            <button type="submit" onclick="alert('Basket Updated Successfully')" class="product-button">Add to Basket</button>
 
-    <div class="product">
-        <img width="200px" src="images/facebook.png" alt="product image">
-        <h2>product title </h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-        <p2>cost </p2>
-        <p3>category</p3>
-        <form id="item" method="post"> </form>
-    </div>
-
-
-
+            </form>
+        </div>
+        @endforeach
 </div>
 
     @include('nav&footer/footer')
