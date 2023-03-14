0162406ad51c532fb54673785dfc9366ba16841b
Raheel18 <116800689+Raheel18@users.noreply.github.com>
2023-03-14 15:19:01 +0000

    Changes to contact page

--- team33/resources/views/contact.blade.php
+++ team33/resources/views/contact.blade.php
@@ -1,34 +1,100 @@
-<!-- <link href="{{ asset('css/app.css?v=').time()}}" rel="stylesheet" type="text/css"> -->
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css?v=').time()}}" rel="stylesheet" type="text/css">
-    <title>Contact Us</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css?v=').time()}}" rel="stylesheet" type="text/css">
+  <title>Contact Us</title>
+
+  <style>
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+
+    form {
+      width: 90%; 
+      max-width: 800px;
+      box-sizing: border-box;
+      padding: 50px;
+      background-color: #f5f5f5;
+      border: 1px solid #ddd;
+      border-radius: 5px;
+      margin-top: 20px; 
+    }
+
+    h1 {
+      text-align: center;
+      color: black;
+    }
+
+    .form-group {
+      margin-bottom: 20px;
+    }
+
+    label {
+      display: block;
+      margin-bottom: 5px;
+      font-weight: 600;
+      color: black; 
+    }
+
+    input[type="text"],
+    input[type="email"],
+    textarea {
+      width: 100%;
+      padding: 10px;
+      font-size: 16px;
+      border-radius: 5px;
+      border: none;
+      background-color: #eee;
+      color: black; 
+    }
+
+    input[type="submit"] {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #000;
+      color: #fff;
+      border: none;
+      border-radius: 5px;
+      font-size: 16px;
+      cursor: pointer;
+    }
+
+    input[type="submit"]:hover {
+      background-color: #333;
+    }
+  </style>
 </head>
+
 <body>
-    @include('nav&footer/nav')
+  @include('nav&footer/nav')
 
+  <form>
     <h1>Contact Us</h1>
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" required>
+    </div>
 
-    <form action = "{{url('/contactForm')}}" class="form-container">
-   @csrf
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" required>
+    </div>
 
-       <label for="email">Email</label>
-       <input type="text" id="email" placeholder="Enter Email" class="box" name="email1" required = "Please Insert Email">
+    <div class="form-group">
+      <label for="query">Query</label>
+      <textarea name="query" id="query" required></textarea>
+    </div>
 
-       <label for="name">Name</label>
-       <input type="text" id="name" placeholder="Name" class="box" name="name1" required = "Please Insert Name">
+    <div class="form-group">
+      <input type="submit" value="Send">
+    </div>
+  </form>
 
-       <label for="query">Query</label>
-       <input type="text" id="query" placeholder="Query" class="box" name="query1" required = "Please Insert Your Query">
-
-       <button type="submit" onclick="alert('Query Sent Successfully')" class="btn">Send Message</button>
-       
-   </form>
-
-    @include('nav&footer/footer')
+  @include('nav&footer/footer')
 </body>
+
 </html>
